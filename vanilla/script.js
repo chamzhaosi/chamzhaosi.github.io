ff05d1d844857ec0fe9bc8ba5a770a9e528bfa56
= <=>
2024-04-07 14:13:51 +0800

    experience web and mobile done

--- vanilla/script.js
+++ vanilla/script.js
@@ -1,3 +1,14 @@
+// Function to check if a section is in the viewport
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
 document.addEventListener('DOMContentLoaded', function() {
 
     window.addEventListener('scroll', function() {
@@ -14,6 +25,9 @@ document.addEventListener('DOMContentLoaded', function() {
     const container = document.getElementById('working_container');
   
     container.addEventListener('wheel', function(e) {
+        if (window.innerWidth < 768){
+            return
+        }
         // Store the initial scroll position
         const initialScrollLeft = container.scrollLeft;
       
@@ -27,5 +41,33 @@ document.addEventListener('DOMContentLoaded', function() {
         }
         
       }, { passive: false }); // Note: The passive option is necessary to be able to preventDefault inside wheel event listeners.
+
+      const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+      const sections = document.querySelectorAll('section');
+  
+      let lastId;
+      let cur = [];
+  
+      // Listen for scroll events on the window
+      window.addEventListener('scroll', (e) => {
+          const scrollY = window.scrollY;
+  
+          sections.forEach((section) => {
+              const sectionTop = section.offsetTop - 90; // Adjust based on your header size
+              const sectionHeight = section.offsetHeight;
+              const sectionId = section.getAttribute('id');
+  
+              if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+                  cur = [sectionId];
+              }
+          });
+  
+          navLinks.forEach((link) => {
+              link.classList.remove('active');
+              if (cur.length && link.getAttribute('href').includes(cur[0])) {
+                  link.classList.add('active');
+              }
+          });
+      });
 
 });
