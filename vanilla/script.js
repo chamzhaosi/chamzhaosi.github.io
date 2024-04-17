c69fd9dfa269d48357552f04c1d0b85b22a7174d
= <=>
2024-04-17 23:22:59 +0800

    amend some layout and design

--- vanilla/script.js
+++ vanilla/script.js
@@ -1,4 +1,4 @@
-function pauseVideo(video_id_name){
+function pauseVideo(video_id_name) {
   document.getElementById(video_id_name).pause();
 }
 
@@ -18,7 +18,6 @@ async function addAndRemoveClassSequentially(selector, className, delayTime) {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-
   // only when the user scoll down start the initial page, then the navigation bar just appear
   window.addEventListener("scroll", function () {
     var scrollPosition = window.scrollY;
@@ -34,27 +33,62 @@ document.addEventListener("DOMContentLoaded", function () {
   const container = document.getElementById("working_container");
 
   // When user scroll down in the working experience section, it will become horizontally scroll
-  container.addEventListener(
-    "wheel",
-    function (e) {
-      // if mobile size view, then not need to horizontal scoll
-      if (window.innerWidth < 768) {
-        return;
-      }
-      // Store the initial scroll position
-      const initialScrollLeft = container.scrollLeft;
+  // container.addEventListener(
+  //   "wheel",
+  //   function (e) {
+  //     // if mobile size view, then not need to horizontal scoll
+  //     if (window.innerWidth < 768) {
+  //       return;
+  //     }
+  //     // Store the initial scroll position
+  //     const initialScrollLeft = container.scrollLeft;
 
-      // Attempt to scroll horizontally: positive deltaY scrolls right, negative scrolls left
-      container.scrollLeft += e.deltaY;
+  //     // Attempt to scroll horizontally: positive deltaY scrolls right, negative scrolls left
+  //     container.scrollLeft += e.deltaY;
 
-      // Check if the scroll position has changed after the attempt
-      if (!(container.scrollLeft === initialScrollLeft)) {
-        // Prevent default scroll behavior (vertical scrolling) if we were able to scroll horizontally
-        e.preventDefault();
-      }
-    },
-    { passive: false }
-  ); // Note: The passive option is necessary to be able to preventDefault inside wheel event listeners.
+  //     // Check if the scroll position has changed after the attempt
+  //     if (!(container.scrollLeft === initialScrollLeft)) {
+  //       // Prevent default scroll behavior (vertical scrolling) if we were able to scroll horizontally
+  //       e.preventDefault();
+  //     }
+  //   },
+  //   { passive: false }
+  // ); // Note: The passive option is necessary to be able to preventDefault inside wheel event listeners.
+
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+
+  container.addEventListener("mousedown", (e) => {
+    isDown = true;
+    container.classList.remove('cur-grab')
+    container.classList.add('cur-grabbing')
+    // console.log(e.pageX) // cursor's x ordinate
+    // console.log(container.offsetLeft) // container/section the starting point
+    startX = e.pageX - container.offsetLeft;
+    scrollLeft = container.scrollLeft;
+  });
+
+  container.addEventListener("mouseleave", () => {
+    isDown = false;
+    container.classList.add('cur-grab')
+    container.classList.remove('cur-grabbing')
+  });
+
+  container.addEventListener("mouseup", () => {
+    isDown = false;
+    container.classList.add('cur-grab')
+    container.classList.remove('cur-grabbing')
+  });
+
+  container.addEventListener("mousemove", (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - container.offsetLeft;
+    const walk = (x - startX) * 2; // Multiply by 3 for faster scrolling (why need to minus, because want to know the how far cursur has moves.)
+    container.scrollLeft = scrollLeft - walk;
+    // console.log(container.scrollLeft)
+  });
 
   const navLinks = document.querySelectorAll(".navbar-nav .nav-link");
   const sections = document.querySelectorAll("section");
@@ -67,7 +101,7 @@ document.addEventListener("DOMContentLoaded", function () {
   window.addEventListener("scroll", (e) => {
     const scrollY = window.scrollY;
 
-    // Calculation current view, and record into a variable cur 
+    // Calculation current view, and record into a variable cur
     sections.forEach((section) => {
       const sectionTop = section.offsetTop - 90; // Adjust based on your header size
       const sectionHeight = section.offsetHeight;
@@ -87,7 +121,6 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
-
   // For word cloud animation
   // Run at the page is loaded (one time only)
   addAndRemoveClassSequentially("li > span", "span-hover", 500);
@@ -97,16 +130,14 @@ document.addEventListener("DOMContentLoaded", function () {
     addAndRemoveClassSequentially("li > span", "span-hover", 500);
   }, 12000);
 
-  
   // Once the video modal is close, the video must be pause
-  const ticketing_video_modal = document.getElementById('ticketingModal');
-  ticketing_video_modal.addEventListener('hidden.bs.modal', function () {
-    pauseVideo('ticketing_video')
+  const ticketing_video_modal = document.getElementById("ticketingModal");
+  ticketing_video_modal.addEventListener("hidden.bs.modal", function () {
+    pauseVideo("ticketing_video");
   });
 
-
-  const fyp_video_modal = document.getElementById('videoModal');
-  fyp_video_modal.addEventListener('hidden.bs.modal', function () {
-    pauseVideo('fyp_video')
+  const fyp_video_modal = document.getElementById("videoModal");
+  fyp_video_modal.addEventListener("hidden.bs.modal", function () {
+    pauseVideo("fyp_video");
   });
 });
