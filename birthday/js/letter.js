d3b2df8530b7a84c131f801c7412df155ffa8ab9
cham <chamzhaosi@gmail.com>
2025-03-29 22:28:56 +0800

    add letter

---
+++ birthday/js/letter.js
@@ -0,0 +1,6 @@
+const draft_letter = document.getElementById("draft_letter");
+const letter = document.getElementById("letter");
+
+// draft_letter.addEventListener("mouseenter", function () {
+//   letter.style.display = "block";
+// });
--- birthday/js/love.js
+++ birthday/js/love.js
@@ -11,6 +11,18 @@ document.getElementById("click").addEventListener("change", function () {
 function createLove(count, container) {
   container.innerHTML = ""; // Clear previous love
   const variant = ["fa-beat", "fa-beat-fade", "fa-bounce", "fa-fade"];
+  const loveColors = [
+    "rgb(220, 20, 60)", // Crimson Red
+    "rgb(255, 0, 0)", // Bright Red
+    "rgb(255, 20, 147)", // Deep Pink
+    "rgb(139, 0, 0)", // Deep Red
+    "rgb(255, 105, 180)", // Hot Pink
+    "rgb(128, 0, 32)", // Burgundy
+    "rgb(174, 65, 101)", // Dark Rose
+    "rgb(255, 36, 0)", // Scarlet Red
+    "rgb(224, 17, 95)", // Ruby Red
+    "rgb(255, 160, 180)", // Blush Pink
+  ];
   let translateX = -130; // Start value
   let step = (220 + 130) / count; // Ensure each step increases
 
@@ -18,8 +30,10 @@ function createLove(count, container) {
     const love = document.createElement("i");
     const span = document.createElement("span");
     const variant_index = Math.round(Math.random() * variant.length);
+    const loveColors_index = Math.round(Math.random() * loveColors.length);
     span.classList.add("love");
     love.classList.add("fa-solid", "fa-heart", variant[variant_index - 1]);
+    love.style.color = loveColors[loveColors_index - 1];
     span.appendChild(love);
 
     // Random values for throwing effect
@@ -29,7 +43,6 @@ function createLove(count, container) {
     let scale = Math.random() * 1.5 + 1.8; // Scale between 1 to 1.5
     let rotate = Math.random() * 90 - 45; // Rotate between -45deg to 45deg
 
-    console.log(translateX);
     // Set animation dynamically
     span.style.animation = `throw-wish-${i} 1.8s linear forwards`;
     span.style.animationPlayState = "paused";
@@ -48,14 +61,13 @@ function createLove(count, container) {
             transform: translateY(${translateY / 2}px) translateX(${
         translateX / 2
       }px) scale(${scale}) rotate(${rotate / 2}deg);
-      z-index: -1;
-
+            z-index: -1;
           }
           100% {
             transform: translateY(${translateY_bottom}px) translateX(${translateX}px) scale(${scale}) rotate(${rotate}deg);
             z-index: 100;
-                  opacity: 1;
-        }
+            opacity: 1;
+          }
         }
       `,
       styleSheet.cssRules.length
